ee2408ee49d5770f6eb37d115bddaecb2fac3b56
Shubham <Shubhamcse12@gmail.com>
2022-11-26 17:35:14 +0530

    initial commit

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r, a;
+    printf("Enter the value of radius =");
+    scanf("%f", &r);
+    a = 3.14*r*r;
+    printf("Area of circle is %f", a);
+    return 0;
+
+}
