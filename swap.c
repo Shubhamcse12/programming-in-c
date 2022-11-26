b8ac9587d5ff1e360998db390e900a14894a8179
Shubham <Shubhamcse12@gmail.com>
2022-11-26 17:47:48 +0530

    initial commit

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("a=");
+    scanf("%d",&a);
+    printf("b=");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("a=%d & b=%d",a,b);
+    return 0;
+}
