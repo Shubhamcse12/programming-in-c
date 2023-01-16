4a629fe43ce77ea9006a25a2c4231d59679026e7
Shubham <Shubhamcse12@gmail.com>
2023-01-16 19:16:28 +0530

    initial commit

--- leap.c
+++ leap.c
@@ -1,24 +1,17 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     int year;
     printf("year =");
-    scanf("%d",&year);
-    if (year%400==0 && year%100==0 || year%4==0)
+    scanf("%d", &year);
+    if (year % 400 == 0 && year % 100 == 0 || year % 4 == 0)
     {
-        printf("%d is a leap year",year);
+        printf("%d is a leap year", year);
     }
-    // else if (year%100==0 && year%400!=0)
-    // {
-    //     printf("%d is a not leap year",year);
-    // }
-    // else if (year%4==0 && year%100!=0)
-    // {
-    //     printf("%d is a leap year",year);
-    // }
+
     else
-    
-        printf("%d is a not leap year",year);
-    
-    
+
+        printf("%d is a not leap year", year);
+
     return 0;
 }
--- p_no_neg.c
+++ p_no_neg.c
@@ -3,13 +3,18 @@ int main(){
     int n;
     printf("n=");
     scanf("%d",&n);
+    if(n==0)
+    printf("number %d is zero\n",n);
+
+    else{
     if (n<0)
     {
-        printf("number %d is negative",n);
+        printf("number %d is negative\n",n);
     }
     else
     {
-        printf("number %d is positive",n);
+        printf("number %d is positive\n",n);
+    }
     }
     return 0;
     
