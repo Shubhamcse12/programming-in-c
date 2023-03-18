125236f3a69fa108377c4c5d3f1a55b11249cf10
Shubham <Shubhamcse12@gmail.com>
2023-03-18 16:27:26 +0530

    initial commit

---
+++ addrec.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int sum(int n){
+    if(n<1)
+    return n;
+    return n+sum(n-1);
+}
+int main(){
+    int x;
+    printf("x=");
+    scanf("%d",&x);
+    int sum(int x);
+    printf("sum is %d\n",sum(x));
+    return 0;
+}
