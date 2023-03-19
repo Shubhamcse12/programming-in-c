5adbaa6e8bc85ffb7ab51c79bcaa0c5f8cc1bafa
SHUBHAM <shubhamcse12@gmail.com>
2023-03-20 00:04:42 +0530

    initial commit

--- acor.c
+++ acor.c
@@ -6,7 +6,7 @@ int main(){
     printf("breadth is =");
     scanf("%f",&b);
     a=l*b;
-    p=2*(l+b),
+    p=2*(l+b);
     printf("area is =%f",a);
     printf("\nperimeter is =%f",p);
     return 0;
