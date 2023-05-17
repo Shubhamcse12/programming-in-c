a4bc2a8179651041708281a2526d2e81312ceb38
SHUBHAM <shubhamcse12@gmail.com>
2023-05-18 02:50:42 +0530

    initial commit

--- addrec.c
+++ addrec.c
@@ -2,6 +2,7 @@
 int sum(int n){
     if(n<1)
     return n;
+    else
     return n+sum(n-1);
 }
 int main(){
--- test.c
+++ test.c
@@ -1,45 +1,9 @@
 #include<stdio.h>
 int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for (int j = n; j > n-i+1; j--)
-        {
-            printf("%d ",j);
-        }
-        for (int j = 0; j < n-i+1; j++)
-        {
-            printf("%d ",n-i+1);
-        }
-        for (int j = n; j < 2*n-i; j++)
-        {
-            printf("%d ",n-i+1);
-        }
-        for (int j = i; j > 1; j--)
-        {
-            printf("%d ",n-j+2);
-        }  
-        printf("\n");
-    } 
-    for(int i=n-1;i>=1;i--){
-        for (int j = n; j > n-i+1; j--)
-        {
-            printf("%d ",j);
-        }
-        for (int j = 0; j < n-i+1; j++)
-        {
-            printf("%d ",n-i+1);
-        }
-        for (int j = n; j < 2*n-i; j++)
-        {
-            printf("%d ",n-i+1);
-        }
-        for (int j = i; j > 1; j--)
-        {
-            printf("%d ",n-j+2);
-        }  
-        printf("\n");
-    }
-    
+    char name ='a';
+    char *ptr;
+    ptr=&name;
+    printf("%d\n",ptr);
+    printf("%c\n",*ptr);
     return 0;
 }
